8efd8541756cb9d03c04fe1e74073f80d27e95af
Matheus Vieira <mvdias20@gmail.com>
2021-03-02 11:10:25 -0300

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
 
-function App() {
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Olá Mundo
     </div>
-  );
+  )
 }
-
-export default App;
